58dd0864a638b78887fa319624bd3957a5932832
Julian <julian.weinelt@outlook.de>
2025-03-30 19:17:45 +0200

    Created javalin webserver

--- common/src/main/java/de/julianweinelt/caesar/core/webserver/WebServer.java
+++ common/src/main/java/de/julianweinelt/caesar/core/webserver/WebServer.java
@@ -1,7 +1,13 @@
 package de.julianweinelt.caesar.core.webserver;
 
+import io.javalin.Javalin;
+
 public class WebServer {
     public void start() {
+        Javalin app = Javalin.create()
+                .post("/auth", context -> {
 
+                })
+                .start(6565);
     }
 }

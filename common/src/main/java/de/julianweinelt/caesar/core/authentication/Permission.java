1e049acb86a0d1083be80d984515c1e4b257f582
Julian Weinelt <julian.weinelt@outlook.de>
2025-03-30 21:45:49 +0200

    Added basic permissions

---
+++ common/src/main/java/de/julianweinelt/caesar/core/authentication/Permission.java
@@ -0,0 +1,23 @@
+package de.julianweinelt.caesar.core.authentication;
+
+public class Permission {
+    public static final String PANEL_VIEW_SERVICES = "panel.view.services";
+    public static final String PANEL_VIEW_TASKS = "panel.view.tasks";
+    public static final String PANEL_VIEW_MODULES = "panel.view.modules";
+    public static final String PANEL_VIEW_CLUSTERS = "panel.view.clusters";
+    public static final String PANEL_VIEW_PLUGINS = "panel.view.plugins";
+    public static final String PANEL_VIEW_SETTINGS = "panel.view.settings";
+    public static final String PANEL_VIEW_FTP = "panel.view.ftp";
+    public static final String PANEL_EDIT_TASKS = "panel.edit.tasks";
+    public static final String PANEL_EDIT_MODULES = "panel.edit.modules";
+    public static final String PANEL_EDIT_PLUGINS = "panel.edit.plugins";
+    public static final String CLOUD_SERVICE_RESTART = "cloud.service.restart";
+    public static final String CLOUD_SERVICE_START = "cloud.service.start";
+    public static final String CLOUD_SERVICE_STOP = "cloud.service.stop";
+    public static final String CLOUD_TASK_CREATE = "cloud.task.create";
+    public static final String CLOUD_TASK_ADD = "cloud.task.add";
+    public static final String CAESAR_ADD_USER = "caesar.add.user";
+    public static final String CAESAR_REMOVE_USER = "caesar.remove.user";
+    public static final String CAESAR_EDIT_USER = "caesar.edit.user";
+    public static final String CAESAR_EDIT_CONFIG = "caesar.edit.config";
+}

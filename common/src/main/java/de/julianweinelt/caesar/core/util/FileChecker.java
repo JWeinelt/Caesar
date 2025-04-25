d658032e692acb7c097e8709bd093fdad32aa582
Julian Weinelt <132051452+JWeinelt@users.noreply.github.com>
2025-04-25 08:27:02 +0200

    Delete common directory

--- common/src/main/java/de/julianweinelt/caesar/core/util/FileChecker.java
+++
@@ -1,9 +0,0 @@
-package de.julianweinelt.caesar.core.util;
-
-import java.io.File;
-
-public class FileChecker {
-    public static boolean isFirstStart() {
-        return new File(".", "config.json").exists();
-    }
-}
--- common/src/main/java/de/julianweinelt/caesar/core/util/logging/LoggerAdapter.java
+++
@@ -1,7 +0,0 @@
-package de.julianweinelt.caesar.core.util.logging;
-
-public interface LoggerAdapter {
-    void info(String msg);
-    void warn(String msg);
-    void error(String msg);
-}

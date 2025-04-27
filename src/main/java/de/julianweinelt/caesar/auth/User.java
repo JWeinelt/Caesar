91837695d2dbc370c068b093ed46e548b94954b3
Julian Weinelt <julian.weinelt@outlook.de>
2025-04-27 20:05:35 +0200

    Added unique id for db sync; added constructor

--- src/main/java/de/julianweinelt/caesar/auth/User.java
+++ src/main/java/de/julianweinelt/caesar/auth/User.java
@@ -1,12 +1,28 @@
 package de.julianweinelt.caesar.auth;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+@Getter
 public class User {
+    private final UUID uuid;
     private String username;
     private int password;
     private final List<String> permissions = new ArrayList<>();
 
     private String discordID;
+
+    public User(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public User(UUID uuid, String username, int password, String discordID) {
+        this.uuid = uuid;
+        this.username = username;
+        this.password = password;
+        this.discordID = discordID;
+    }
 }

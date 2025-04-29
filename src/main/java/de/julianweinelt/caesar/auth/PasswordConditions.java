ed54529bce9992d6c8ce43cb94ef3dd5ba77b823
Julian Weinelt <julian.weinelt@outlook.de>
2025-04-29 20:22:32 +0200

    Added configurable password conditions

---
+++ src/main/java/de/julianweinelt/caesar/auth/PasswordConditions.java
@@ -0,0 +1,40 @@
+package de.julianweinelt.caesar.auth;
+
+import de.julianweinelt.caesar.util.StringUtil;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PasswordConditions {
+    private int minPasswordLength = 4;
+    private int maxPasswordLength = 32;
+    private int passwordHistory = 0;
+    private boolean enforceUppercase = true;
+    private boolean enforceNumbers = true;
+    private boolean enforceSymbols = false;
+
+    public PasswordConditions(int minPasswordLength, int maxPasswordLength, int passwordHistory,
+                              boolean enforceUppercase, boolean enforceNumbers, boolean enforceSymbols) {
+        this.minPasswordLength = minPasswordLength;
+        this.maxPasswordLength = maxPasswordLength;
+        this.passwordHistory = passwordHistory;
+        this.enforceUppercase = enforceUppercase;
+        this.enforceNumbers = enforceNumbers;
+        this.enforceSymbols = enforceSymbols;
+    }
+
+    public PasswordConditions() {}
+
+    public boolean checkPassword(String password) {
+        String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+        String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String DIGITS = "0123456789";
+        String SYMBOLS = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+        return
+                StringUtil.contains(password, LOWERCASE) &&
+                enforceUppercase && StringUtil.contains(password, LETTERS) || !enforceUppercase
+                && !StringUtil.contains(password, DIGITS) && enforceNumbers || !enforceNumbers
+                && !StringUtil.contains(password, SYMBOLS) && enforceSymbols || !enforceSymbols;
+    }
+}

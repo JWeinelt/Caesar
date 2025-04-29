6ad8030674db9d88324e1d60562a26627bc20f4f
Julian Weinelt <julian.weinelt@outlook.de>
2025-04-29 20:20:30 +0200

    Added server connections

---
+++ src/main/java/de/julianweinelt/caesar/integration/ServerConnection.java
@@ -0,0 +1,22 @@
+package de.julianweinelt.caesar.integration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ServerConnection {
+    private final String name;
+    private String address;
+    private int port;
+
+    public ServerConnection(String name) {
+        this.name = name;
+    }
+
+    public ServerConnection(String name, String address, int port) {
+        this.name = name;
+        this.address = address;
+        this.port = port;
+    }
+}

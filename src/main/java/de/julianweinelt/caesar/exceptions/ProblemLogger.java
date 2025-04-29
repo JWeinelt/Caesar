16f2e61c826d5eb2b394ef50255ebd4effafe844
Julian Weinelt <julian.weinelt@outlook.de>
2025-04-29 20:21:01 +0200

    Added modern logging

---
+++ src/main/java/de/julianweinelt/caesar/exceptions/ProblemLogger.java
@@ -0,0 +1,28 @@
+package de.julianweinelt.caesar.exceptions;
+
+import de.julianweinelt.caesar.Caesar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProblemLogger {
+
+    public static ProblemLogger getInstance() {
+        return Caesar.getInstance().getProblemLogger();
+    }
+
+    public void log(ProblemType type, String message) {
+        logs.add(new LogEntry(type, message));
+    }
+
+    private final List<LogEntry> logs = new ArrayList<>();
+
+    public record LogEntry(ProblemType type, String message) {}
+
+    public enum ProblemType {
+        WARN,
+        ERROR,
+        FATAL,
+        DEBUG;
+    }
+}

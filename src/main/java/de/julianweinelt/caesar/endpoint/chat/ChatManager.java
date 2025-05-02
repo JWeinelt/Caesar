06dbf64426267d5f38ea6c4b5654776df150fd55
Julian Weinelt <julian.weinelt@outlook.de>
2025-05-02 22:43:15 +0200

    Added chat manager

---
+++ src/main/java/de/julianweinelt/caesar/endpoint/chat/ChatManager.java
@@ -0,0 +1,19 @@
+package de.julianweinelt.caesar.endpoint.chat;
+
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ChatManager {
+    private final List<Chat> chats = new ArrayList<>();
+
+    @Setter
+    private ChatServer server;
+
+    public Chat getChat(UUID uuid) {
+        for (Chat c : chats) if (c.getUniqueID().equals(uuid)) return c;
+        return null;
+    }
+}

d8202378630b7e9b057c84ec1f24b03cc9d7792c
Julian Weinelt <julian.weinelt@outlook.de>
2025-05-02 16:22:14 +0200

    Added discord bot

--- src/main/java/de/julianweinelt/caesar/discord/DiscordBot.java
+++ src/main/java/de/julianweinelt/caesar/discord/DiscordBot.java
@@ -1,5 +1,6 @@
 package de.julianweinelt.caesar.discord;
 
+import de.julianweinelt.caesar.Caesar;
 import de.julianweinelt.caesar.storage.LocalStorage;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
@@ -13,11 +14,26 @@ import java.util.Arrays;
 public class DiscordBot {
     private static final Logger log = LoggerFactory.getLogger(DiscordBot.class);
 
+    public static DiscordBot getInstance() {
+        return Caesar.getInstance().getDiscordBot();
+    }
+
     private JDA jda;
 
     public void start() {
         JDABuilder builder = JDABuilder.create(LocalStorage.getInstance().getData().getDiscordBotToken(),
                 Arrays.asList(GatewayIntent.values()));
+        jda = builder.build();
+    }
 
+    public void stop() {
+        if (jda != null) {
+            jda.shutdown();
+        }
+    }
+
+    public void restart() {
+        stop();
+        start();
     }
 }

948c8dd3dee45167887afb425015a32fe14902b1
Julian Weinelt <julian.weinelt@outlook.de>
2025-05-02 16:21:59 +0200

    Added possibility for corporate designs

---
+++ src/main/java/de/julianweinelt/caesar/endpoint/CorporateDesign.java
@@ -0,0 +1,5 @@
+package de.julianweinelt.caesar.endpoint;
+
+import java.awt.*;
+
+public record CorporateDesign(Color background, Color front, Color buttons, boolean allowBackgrounds, String logoURL) {}

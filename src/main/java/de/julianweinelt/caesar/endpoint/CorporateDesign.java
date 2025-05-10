65a235f3de8e12a087ed47d88796f84844e2e368
Julian Weinelt <julian.weinelt@outlook.de>
2025-05-11 00:09:57 +0200

    Added DEFAULT CorporateDesign

--- src/main/java/de/julianweinelt/caesar/endpoint/CorporateDesign.java
+++ src/main/java/de/julianweinelt/caesar/endpoint/CorporateDesign.java
@@ -1,5 +1,15 @@
 package de.julianweinelt.caesar.endpoint;
 
+import de.julianweinelt.caesar.util.DatabaseColorParser;
+
 import java.awt.*;
 
-public record CorporateDesign(String background, String front, String buttons, boolean allowBackgrounds, String logoURL) {}
+public record CorporateDesign(String background, String front, String buttons, boolean allowBackgrounds, String logoURL) {
+    public static final CorporateDesign DEFAULT = new CorporateDesign(
+            DatabaseColorParser.parseColor(Color.BLUE),
+            DatabaseColorParser.parseColor(Color.BLUE),
+            DatabaseColorParser.parseColor(Color.BLUE),
+            true,
+            "http://caesarnet.cloud/logo.png");
+
+}

9697d404680ef84b2af21592b9c41530e2caae7c
Julian Weinelt <julian.weinelt@outlook.de>
2025-05-11 00:10:59 +0200

    First action for testing: Blocked words (auto-mod)

---
+++ src/main/java/de/julianweinelt/caesar/action/predefined/DiscordPlayerBlockWordAction.java
@@ -0,0 +1,25 @@
+package de.julianweinelt.caesar.action.predefined;
+
+import de.julianweinelt.caesar.action.Action;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.JDA;
+
+public class DiscordPlayerBlockWordAction implements Action {
+    @Override
+    public void run(Object... params) {
+        if (params.length != 4) {
+            throw new IllegalArgumentException("Not enough parameters for DiscordPlayerBlockWordAction.");
+        }
+
+        JDA jda = (JDA) params[0];
+        Member member = (Member) params[1];
+        String memberID = (String) params[2];
+        String word = (String) params[3];
+
+        // Logik für das Blockieren eines Wortes
+        System.out.println("Das blockierte Wort wurde von " + member.getEffectiveName() + " verwendet: " + word);
+
+        // Hier kann man die weiteren Discord-Aktionen einfügen, z.B. das Entfernen der Nachricht.
+        // jda.getTextChannelById(channelId).deleteMessageById(messageId).queue();
+    }
+}

b94da4c7303ddefb16e62ed392c9d6608e8ded05
Julian Weinelt <julian.weinelt@outlook.de>
2025-06-17 16:12:12 +0200

    Improved saving of embed message templates

--- src/main/java/de/julianweinelt/caesar/util/wrapping/DiscordEmbedWrapper.java
+++ src/main/java/de/julianweinelt/caesar/util/wrapping/DiscordEmbedWrapper.java
@@ -2,12 +2,17 @@ package de.julianweinelt.caesar.util.wrapping;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import de.julianweinelt.caesar.util.DatabaseColorParser;
 import lombok.Getter;
 import lombok.Setter;
+import net.dv8tion.jda.api.EmbedBuilder;
+
+import java.time.temporal.TemporalAccessor;
 
 @Setter
 @Getter
 public class DiscordEmbedWrapper {
+    private final String embedID;
     private String title;
     private String description;
     private String url;
@@ -20,10 +25,13 @@ public class DiscordEmbedWrapper {
 
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
-    public DiscordEmbedWrapper() {}
+    public DiscordEmbedWrapper(String embedID) {
+        this.embedID = embedID;
+    }
 
-    public DiscordEmbedWrapper(String title, String description, String url, String color,
+    public DiscordEmbedWrapper(String embedID, String title, String description, String url, String color,
                                String footer, String image, String thumbnail, String timestamp, String author) {
+        this.embedID = embedID;
         this.title = title;
         this.description = description;
         this.url = url;
@@ -41,5 +49,17 @@ public class DiscordEmbedWrapper {
 
     public static DiscordEmbedWrapper fromJson(String json) {
         return GSON.fromJson(json, DiscordEmbedWrapper.class);
+    }
+
+    public EmbedBuilder toEmbed() {
+        return new EmbedBuilder()
+                .setTitle(this.title)
+                .setDescription(this.description)
+                .setUrl(this.url)
+                .setColor(DatabaseColorParser.parseColor(this.color))
+                .setFooter(this.footer)
+                .setImage(this.image)
+                .setThumbnail(this.thumbnail)
+                .setAuthor(this.author);
     }
 }

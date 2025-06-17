cbb2250a47868a4e0253ba8d2baf123ea9c4bc89
Julian Weinelt <julian.weinelt@outlook.de>
2025-06-17 12:46:03 +0200

    Updated external APIs

--- src/main/java/de/julianweinelt/caesar/endpoint/minecraft/MEndpointCurseForge.java
+++ src/main/java/de/julianweinelt/caesar/endpoint/minecraft/MEndpointCurseForge.java
@@ -2,6 +2,6 @@ package de.julianweinelt.caesar.endpoint.minecraft;
 
 public class MEndpointCurseForge extends MCPluginEndpoint{
     protected MEndpointCurseForge(String name, String baseURL, String apiURLPart, boolean needKey, String apiKey, boolean acceptsDownload) {
-        super(name, baseURL, apiURLPart, needKey, apiKey, acceptsDownload);
+        super("CurseForge", "https://api.curseforge.com", "/", true, "", true);
     }
 }

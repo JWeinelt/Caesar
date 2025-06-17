db9434a0eb7b712b5abb54ad44d9cf92ea53fc54
Julian Weinelt <julian.weinelt@outlook.de>
2025-06-17 16:22:57 +0200

    Fix(Wrong constructor)

--- src/main/java/de/julianweinelt/caesar/endpoint/plugins/MinecraftPlugin.java
+++ src/main/java/de/julianweinelt/caesar/endpoint/plugins/MinecraftPlugin.java
@@ -1,9 +1,18 @@
 package de.julianweinelt.caesar.endpoint.plugins;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
 public class MinecraftPlugin {
     private final UUID uniqueID;
     private float rating;
     private VersionCompatibility compatibility;
+
+    public MinecraftPlugin(UUID uniqueID) {
+        this.uniqueID = uniqueID;
+    }
 }

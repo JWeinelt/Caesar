855d3c4557996b5e084c43657850ee4e929d4f5f
Julian Weinelt <julian.weinelt@outlook.de>
2025-06-17 16:10:22 +0200

    Removed Hangar API class as it does not exist

--- src/main/java/de/julianweinelt/caesar/endpoint/minecraft/MEndpointHangar.java
+++
@@ -1,7 +0,0 @@
-package de.julianweinelt.caesar.endpoint.minecraft;
-
-public class MEndpointHangar extends MCPluginEndpoint{
-    protected MEndpointHangar(String name, String baseURL, String apiURLPart, boolean needKey, String apiKey, boolean acceptsDownload) {
-        super(name, baseURL, apiURLPart, needKey, apiKey, acceptsDownload);
-    }
-}

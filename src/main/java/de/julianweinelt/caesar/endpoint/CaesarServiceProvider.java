a21fc76e4f14f77cae2a91aa989027b86cbf0326
Julian Weinelt <julian.weinelt@outlook.de>
2025-06-18 12:02:44 +0200

    Add CaesarServiceProvider.java with version fetching for updates

--- src/main/java/de/julianweinelt/caesar/endpoint/CaesarServiceProvider.java
+++ src/main/java/de/julianweinelt/caesar/endpoint/CaesarServiceProvider.java
@@ -1,9 +1,55 @@
 package de.julianweinelt.caesar.endpoint;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import de.julianweinelt.caesar.Caesar;
+import de.julianweinelt.caesar.storage.Configuration;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
 public class CaesarServiceProvider {
-    private String apiToken;
+    private static final Logger log = LoggerFactory.getLogger(CaesarServiceProvider.class);
+
+    private final String endpointURI = Configuration.getInstance().getCaesarAPIEndpoint();
+
+    @Getter
+    private String latestClientVersion = "";
+    @Getter
+    private String latestServerVersion = "";
+    @Getter
+    private String latestConnectorVersion = "";
+
+    public static CaesarServiceProvider getInstance() {
+        return Caesar.getInstance().getServiceProvider();
+    }
 
     public void start() {
+        log.info("Starting CaesarServiceProvider...");
+        log.info("Connecting to main endpoint...");
+        log.info("Checking current versions...");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(endpointURI + "versions")).build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(body -> {
+                    JsonObject re =  new JsonParser().parse(body).getAsJsonObject();
+                    latestClientVersion = re.get("client").getAsString();
+                    latestServerVersion = re.get("server").getAsString();
+                    latestConnectorVersion = re.get("connector").getAsString();
+
+                })
+                .exceptionally(ex -> {
+                    log.error("Response (async): {}", ex.getMessage(), ex);
+                    return null;
+                });
 
     }
 }

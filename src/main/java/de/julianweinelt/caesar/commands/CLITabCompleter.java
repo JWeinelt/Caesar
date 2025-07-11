2cc84bb3fe7a01471d7354441dcaab1cf7f07a4b
Julian Weinelt <julian.weinelt@outlook.de>
2025-07-11 12:39:14 +0200

    feat: Add API methods for better tab completion

--- src/main/java/de/julianweinelt/caesar/commands/CLITabCompleter.java
+++ src/main/java/de/julianweinelt/caesar/commands/CLITabCompleter.java
@@ -1,7 +1,26 @@
 package de.julianweinelt.caesar.commands;
 
+import de.julianweinelt.caesar.util.StringUtil;
+
+import java.util.Arrays;
 import java.util.List;
 
-public interface CLITabCompleter {
-    List<String> complete(String[] args);
+public abstract class CLITabCompleter {
+    public abstract List<String> onTabCompletion(String[] args);
+
+
+
+
+    public void complete(List<String> list, String arg, String... completions) {
+        list.clear();
+        StringUtil.copyPartialMatches(arg, Arrays.asList(completions), list);
+    }
+    public void complete(List<String> list, String arg, List<String> completions) {
+        list.clear();
+        StringUtil.copyPartialMatches(arg, completions, list);
+    }
+
+    public void completeEmpty(List<String> list) {
+        list.clear();
+    }
 }

b9ed427be2013e4a797355e63a433f90eedf6fd7
Julian Weinelt <julian.weinelt@outlook.de>
2025-07-24 09:45:43 +0200

    api: Add class which fetches player data from Mojang

---
+++ src/main/java/de/julianweinelt/caesar/endpoint/MinecraftUUIDFetcher.java
@@ -0,0 +1,10 @@
+package de.julianweinelt.caesar.endpoint;
+
+import java.util.UUID;
+
+public class MinecraftUUIDFetcher {
+    public static String getByID(UUID uuid) {
+        //TODO: Add logic
+        return "";
+    }
+}

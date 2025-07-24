a1c44095ff9642a10b8daee8d537bebb76ed6df4
Julian Weinelt <julian.weinelt@outlook.de>
2025-07-24 11:33:00 +0200

    api: Make events non-cancelable

--- src/main/java/de/julianweinelt/caesar/plugin/event/Event.java
+++ src/main/java/de/julianweinelt/caesar/plugin/event/Event.java
@@ -1,6 +1,7 @@
 package de.julianweinelt.caesar.plugin.event;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,11 +10,18 @@ public class Event {
     @Getter
     private final String name;
     private final Map<String, EventProperty> properties = new HashMap<>();
-    @Getter
+    @Getter @Setter
     private boolean cancelled = false;
+    @Getter
+    private boolean cancellable = true;
 
     public Event(String name) {
         this.name = name;
+    }
+
+    public Event nonCancellable() {
+        cancellable = false;
+        return this;
     }
 
     public Event set(String key, Object value) {
@@ -22,11 +30,13 @@ public class Event {
     }
 
     public EventProperty get(String key) {
+        if (!properties.containsKey(key))
+            throw new IllegalArgumentException("The key " + key + " does not exist in the event " + name);
         return properties.get(key);
     }
 
     public void cancel() {
+        if (!cancellable) throw new IllegalStateException("The event " + name + " cannot be cancelled");
         this.cancelled = true;
     }
-
 }

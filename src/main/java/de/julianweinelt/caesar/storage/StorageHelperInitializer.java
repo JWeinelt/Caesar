f5cdf7fecbb12819d9db308ed2312e4d7e218089
Julian Weinelt <julian.weinelt@outlook.de>
2025-07-24 12:29:23 +0200

    chore: Rename permissions

--- src/main/java/de/julianweinelt/caesar/storage/StorageHelperInitializer.java
+++ src/main/java/de/julianweinelt/caesar/storage/StorageHelperInitializer.java
@@ -1,18 +1,27 @@
 package de.julianweinelt.caesar.storage;
 
-import com.google.gson.JsonObject;
 import de.julianweinelt.caesar.discord.ticket.TicketStatus;
 
-import java.awt.*;
+import java.awt.Color;
 import java.util.List;
 import java.util.UUID;
 
 public class StorageHelperInitializer {
     public static final String[] PERMISSIONS = {
-            "caesar.user.create",
-            "caesar.user.delete",
-            "caesar.user.edit",
-            "caesar.user.list",
+            "caesar.admin.user.create",
+            "caesar.admin.user.delete",
+            "caesar.admin.user.view",
+            "caesar.admin.user.edit",
+            "caesar.admin.user.list",
+            "caesar.admin.user.permission.add",
+            "caesar.admin.user.permission.remove",
+            "caesar.admin.user.role.assign",
+            "caesar.admin.role.create",
+            "caesar.admin.role.delete",
+            "caesar.admin.role.edit",
+            "caesar.admin.role.view",
+            "caesar.admin.role.list",
+            "caesar.admin.discord.manage",
             "caesar.view.server",
             "caesar.view.server.services",
             "caesar.view.server.tasks",
@@ -32,22 +41,32 @@ public class StorageHelperInitializer {
             "caesar.server.servers.restart",
             "caesar.server.servers.stop",
             "caesar.server.servers.delete",
-            "caesar.players.add",
+            "caesar.players.create",
             "caesar.players.edit",
-            "caesar.players.remove",
+            "caesar.players.delete",
+            "caesar.players.view",
+            "caesar.players.notes.view",
+            "caesar.players.notes.create",
+            "caesar.players.notes.delete",
             "caesar.process.create",
             "caesar.process.edit",
-            "caesar.process.manage",
-            "caesar.process.changestatus",
+            "caesar.process.change-status",
+            "caesar.process.assign-player",
             "caesar.process.remove",
             "caesar.process.archive",
+            "caesar.process.type.create",
+            "caesar.process.type.delete",
+            "caesar.process.status.create",
+            "caesar.process.status.delete",
             "caesar.tickets.edit",
             "caesar.tickets.remove",
+            "caesar.tickets.archive",
             "caesar.connections.create",
             "caesar.connections.disable",
             "caesar.connections.enable",
-            "caesar.connections.remove",
+            "caesar.connections.delete",
             "caesar.connections.update",
+            "caesar.connections.view",
             "caesar.link.ban",
             "caesar.link.unban",
             "caesar.link.mute",
@@ -59,8 +78,8 @@ public class StorageHelperInitializer {
 
     public static List<TicketStatus> getDefaultTicketStatusList() {
         return List.of(
-                new TicketStatus(UUID.randomUUID(), "OPEN", "Ticket is open", java.awt.Color.GREEN),
-                new TicketStatus(UUID.randomUUID(), "CLOSED", "Ticket is closed", java.awt.Color.RED),
+                new TicketStatus(UUID.randomUUID(), "OPEN", "Ticket is open", Color.GREEN),
+                new TicketStatus(UUID.randomUUID(), "CLOSED", "Ticket is closed", Color.RED),
                 new TicketStatus(UUID.randomUUID(), "ARCHIVED", "Ticket has been archived", Color.GRAY)
         );
     }

a8cb2e8f28c3c80e11430e90c97ecd9d40dda2b0
Julian Weinelt <julian.weinelt@outlook.de>
2025-07-24 11:33:59 +0200

    api: Add placeholder plugin class for system events and commands

---
+++ src/main/java/de/julianweinelt/caesar/plugin/SystemPlugin.java
@@ -0,0 +1,29 @@
+package de.julianweinelt.caesar.plugin;
+
+public final class SystemPlugin extends CPlugin{
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onDefineEvents() {
+
+    }
+
+    @Override
+    public void onCreateCommands() {
+
+    }
+}

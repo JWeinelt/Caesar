70c9d69eee18add32a02cb1faa767d62f6767400
Julian Weinelt <julian.weinelt@outlook.de>
2025-07-30 10:29:01 +0200

    api: mark features as beta and display warnings

---
+++ src/main/java/de/julianweinelt/caesar/annotation/BetaFeature.java
@@ -0,0 +1,9 @@
+package de.julianweinelt.caesar.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.FIELD)
+public @interface BetaFeature {
+}

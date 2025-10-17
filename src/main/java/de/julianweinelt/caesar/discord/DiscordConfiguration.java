41bea676aa7f29b11e8b3503435fffabea1f6ee4
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-17 09:35:24 +0200

    api: field optimization

--- src/main/java/de/julianweinelt/caesar/discord/DiscordConfiguration.java
+++ src/main/java/de/julianweinelt/caesar/discord/DiscordConfiguration.java
@@ -1,12 +1,14 @@
 package de.julianweinelt.caesar.discord;
 
 import de.julianweinelt.caesar.util.wrapping.DiscordEmbedWrapper;
+import de.julianweinelt.caesar.util.wrapping.MessageSentWrapper;
 import lombok.Getter;
 import lombok.Setter;
 import net.dv8tion.jda.api.OnlineStatus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Getter
 @Setter
@@ -36,7 +38,31 @@ public class DiscordConfiguration {
     private String ticketCategory = "1203373647134195733";
     private final List<DiscordEmbedWrapper> embeds = new ArrayList<>();
     private String waitingRoom = "1203373647134195736";
+    private final List<MessageSentWrapper> embedMessages = new ArrayList<>();
 
     private String guild = "1203373645108355072";
     private String infoChannel = "1221472133352525865";
+
+    public Optional<MessageSentWrapper> getMessageByEmbedID(String embedID) {
+        return embedMessages.stream().filter(m -> m.embedID().equals(embedID)).findFirst();
+    }
+    public List<MessageSentWrapper> getMessagesByEmbedID(String embedID) {
+        List<MessageSentWrapper> result = new ArrayList<>();
+        for (MessageSentWrapper m : embedMessages) {
+            if (m.embedID().equals(embedID)) result.add(m);
+        }
+        return result;
+    }
+
+    public DiscordEmbedWrapper getEmbedByID(String embedID) {
+        for (DiscordEmbedWrapper e : embeds) {
+            if (e.getEmbedID().equals(embedID)) return e;
+        }
+        return null;
+    }
+    public void deleteEmbedByID(String embedID) {
+        embeds.removeIf(e -> e.getEmbedID().equals(embedID));
+        embedMessages.removeIf(m -> m.embedID().equals(embedID));
+    }
+
 }
